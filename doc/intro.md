1354ec20fa826b08c8be9a9d96ec2fc7d6397f91
suicodave <suico.dave@gmail.com>
2021-02-05 21:39:07 +0800

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to card_deck
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/card_deck/core_test.clj
@@ -0,0 +1,7 @@
+(ns card-deck.core-test
+  (:require [clojure.test :refer :all]
+            [card-deck.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
